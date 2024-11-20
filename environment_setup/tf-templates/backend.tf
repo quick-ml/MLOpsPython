1049ca7de6219f19c525502cead0b8cb932ea1a6
Ravi <ravi@vytec.com>
2024-11-20 13:07:34 +0530

    fff

--- environment_setup/tf-templates/backend.tf
+++ environment_setup/tf-templates/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "mlops-RG"
-    storage_account_name = "mlopsinfra.tfstate"
+    storage_account_name = "mlopsstatestor"
     container_name       = "tfstate-cont"
     key                  = "mlopsinfra.tfstate"
 
